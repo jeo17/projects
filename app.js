0a8a76377497539e094ad5b7bda2ba7bf2f4c08c
jeo <abdeldjalildh990@gmail.com>
2022-11-08 23:05:29 +0100

    Best Practices and add express js

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+
+
+app.get('/', (req, res) => {
+
+    res.render("wallpaper")
+  })
+  
+  app.get('/login', (req, res) => {
+    res.send('put your username and password !')
+  })
+  
+  app.use( (req, res) => {
+    res.redirect(301,'/login')
+  })
+  
+  
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+  
